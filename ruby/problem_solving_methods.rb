782fcb6483feff7f7c88f229bac7d498a40d4b7c
DylanLovesCoffee <dyang825@gmail.com>
2017-04-29 21:21:50 -0400

    Create fibonacci method to push sum of last index and second-to-last index into array and repeat.

--- ruby/problem_solving_methods.rb
+++ ruby/problem_solving_methods.rb
@@ -3,13 +3,23 @@ my_array = [10, 20, 30, 40, 50]
 def search_array(array, integer)
   index = 0
   array.each do |element|
-      if integer == element
-        puts index
-      else
-        p 'nil'
-        index += 1
-      end
+    if integer == element
+      puts index
+    else
+      p 'nil'
+      index += 1
+    end
   end
 end
 
 search_array(my_array, 40)
+
+def fibonacci(index)
+  fib = 0
+  fib_array = [0, 1, 1]
+  while fib < index
+    sum = fib_array.last + (fib_array[-2])
+    fib_array.push(sum)
+    fib += 1
+  end
+end

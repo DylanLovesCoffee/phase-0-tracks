26d73092dd4fc302e58d18a0ebd1498f1d5ccf23
DylanLovesCoffee <dyang825@gmail.com>
2017-05-02 17:56:34 -0400

    Create mixin Shout module. Create instances and call mixin methods.

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,14 +1,42 @@
+# module Shout
+#
+#   def self.yell_angrily(words)
+#     words + "!!!" + ":("
+#   end
+#
+#   def self.yelling_happily(words)
+#     words.upcase + "!!!!" + ":)"
+#   end
+#
+# end
+#
+# p Shout.yell_angrily("I'm feeling so exhausted")
+# p Shout.yelling_happily("Water is great")
+
 module Shout
 
-  def self.yell_angrily(words)
-    words + "!!!" + ":("
+  def roar(words)
+    puts words.upcase + "!!"
   end
 
-  def self.yelling_happily(words)
-    words.upcase + "!!!!" + ":)"
+  def magic_words
+    puts "You shall not pass!"
+    puts "*hits ground with staff*"
   end
 
 end
 
-p Shout.yell_angrily("I'm feeling so exhausted")
-p Shout.yelling_happily("Water is great")
+
+class Wizard
+  include Shout
+end
+
+class Demon
+  include Shout
+end
+
+gandalf = Wizard.new
+gandalf.magic_words
+
+balrog = Demon.new
+balrog.roar("Hey, get over here")

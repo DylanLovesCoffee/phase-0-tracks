5c174d921b85a6fd8a57b4746011bae5989caea1
DylanLovesCoffee <dyang825@gmail.com>
2017-05-02 20:59:26 -0400

    Pseudocode

---
+++ ruby/gps2_2.rb
@@ -0,0 +1,28 @@
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps:
+  # Split user input into an array
+  # Push each element in the array into a hash
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: [what data type goes here, array or hash?]
+
+# Method to add an item to a list
+# input: list(hash), item name(string), and optional quantity(integer)
+# steps: take item name and quantity and push to list. As key and value
+# output: updated list
+
+# Method to remove an item from the list
+# input: item name
+# steps: check if item name is in list. If item is in list, remove item from the list. If not, ask user for a valid item
+# output: updated list
+
+# Method to update the quantity of an item
+# input: item name, integer
+# steps: check if item name is in list. If item is in the list, update the quantity. If not, ask user for a valid item
+# output: updated list
+
+# Method to print a list and make it look pretty
+# input: list, item, quantities
+# steps: iterate over list
+# output: item name and its quantity

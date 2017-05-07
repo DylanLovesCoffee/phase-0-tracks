8adc0f338de937a7d8ab239cb1a0cd226badccd7
DylanLovesCoffee <dyang825@gmail.com>
2017-05-07 14:36:14 -0400

    Test incorrect answer. Test guess counter increase for unique guesses.

--- ruby/game/game_spec.rb
+++ ruby/game/game_spec.rb
@@ -8,10 +8,20 @@ describe Game do
     expect(game.hidden_answer).to eq [ "_", "_", "_", "_", "_" ]
   end
 
-  it "if a guess is correct, the underscore where the letter belongs will be replaced by said letter" do
+  it "verifies a correct guess, and the hidden answer where the letter belongs will be replaced by that letter" do
     game.check_answer("f")
     expect(game.hidden_answer).to eq [ "f", "_", "_", "_", "_" ]
   end
 
+  it "verifies a guess is incorrect, and the hidden answer will not change" do
+    game.check_answer("r")
+    expect(game.hidden_answer).to eq [ "_", "_", "_", "_", "_" ]
+  end
+
+  it "increases the guess counter by 1 for every unique guess" do
+    game.check_answer("r")
+    game.check_answer("f")
+    expect(game.guess_count).to eq 2
+  end
 
 end

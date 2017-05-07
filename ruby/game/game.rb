7f180d7c13c9e1d9a0caaf64351dff056bf0e4ad
DylanLovesCoffee <dyang825@gmail.com>
2017-05-07 14:47:48 -0400

    Uncommented driver code.

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -35,27 +35,27 @@ class Game
 end
 
 # DRIVER CODE
-# puts 'What word would you like to enter?'
-# input = gets.chomp
-# guesses_left = input.length + 3
-# new_game = Game.new(input)
-#
-# puts "You have #{guesses_left} guesses."
-#
-# while new_game.guess_count != guesses_left
-#   puts "What letter would you like to guess?"
-#   guess = gets.chomp
-#   new_game.check_answer(guess)
-#   p "Guesses Left: #{guesses_left - new_game.guess_count}"
-#   p new_game.hidden_answer
-#
-#   if guess.downcase == new_game.answer.join
-#     puts "You did it!"
-#     break
-#   elsif new_game.hidden_answer == new_game.answer
-#     puts "You did it!"
-#     break
-#   elsif new_game.guess_count == guesses_left
-#     puts "Ya dun goof'd!"
-#   end
-# end
+puts 'What word would you like to enter?'
+input = gets.chomp
+guesses_left = input.length + 3
+new_game = Game.new(input)
+
+puts "You have #{guesses_left} guesses."
+
+while new_game.guess_count != guesses_left
+  puts "What letter would you like to guess?"
+  guess = gets.chomp
+  new_game.check_answer(guess)
+  p "Guesses Left: #{guesses_left - new_game.guess_count}"
+  p new_game.hidden_answer
+
+  if guess.downcase == new_game.answer.join
+    puts "You did it!"
+    break
+  elsif new_game.hidden_answer == new_game.answer
+    puts "You did it!"
+    break
+  elsif new_game.guess_count == guesses_left
+    puts "Ya dun goof'd!"
+  end
+end

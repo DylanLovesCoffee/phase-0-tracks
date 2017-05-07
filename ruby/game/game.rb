bed3fa1c3761dceaee202532eaa1f381b51a63ba
DylanLovesCoffee <dyang825@gmail.com>
2017-05-06 21:28:18 -0400

    Allow a player to see where the letter fits into the hidden answer if they get the guess correct.

--- ruby/game/game.rb
+++ ruby/game/game.rb
@@ -1,29 +1,25 @@
 # Game class
 class Game
-  attr_reader :win, :guess_count
+  attr_accessor :hidden_answer, :guess_count
 
   def initialize(secret_word)
     @guess_count = 0
     @answer = secret_word.downcase.split('')
-    @guess_correct = false
-    @win = false
+    @hidden_answer = ["_"] * secret_word.length
   end
 
   def check_answer(letter)
     @guess_count += 1
-    if @answer.include?("#{letter}")
-      @guess_correct = true
-    elsif @answer == letter
-      @win = true
-    else
-      false
+    letter_index = @answer.index(letter)
+    if @answer.include?("#{letter}") == true
+      @hidden_answer.delete_at(letter_index)
+      @hidden_answer.insert(letter_index, letter)
     end
   end
-
 end
 
 # driver
-puts 'What word or phrase would you like to enter?'
+puts 'What word would you like to enter?'
 input = gets.chomp
 new_game = Game.new(input)
 
@@ -33,8 +29,10 @@ while new_game.guess_count != (input.length + 3)
   puts "What letter would you like to guess?"
   guess = gets.chomp
   if new_game.check_answer(guess)
-    puts "You got one! Keep trying!"
+    p "You got one! Keep trying!"
+    p new_game.hidden_answer
   else
-    puts "Oof, try again."
+    p "Oof, try again."
+    p new_game.hidden_answer
   end
 end

4ed90431c27765e38db47c5b7a7849a6869ab70e
DylanLovesCoffee <dyang825@gmail.com>
2017-05-15 00:52:40 -0400

    Make change so that string_length under random_data function is inside loop. Add driver code to test functions 10 times.

--- js/algos.js
+++ js/algos.js
@@ -1,15 +1,15 @@
-// FIND THE LONGEST PHRASE
+// ----- FIND THE LONGEST PHRASE -----
 // Create a function that takes an array as its parameter. Iterate through each element in the array.
 // Save each elements character length into a separate array.
 // Return the highest number value in terms of character length from that array.
 // Iterate through our original array once again, and if the character length of an element is equal to the highest character length number value, return that element.
 
-// KEY-VALUE MATCH
+// ----- KEY-VALUE MATCH -----
 // Create a function that takes 2 objects as parameters. Loop through each object's key.
 // If any keys match, loop through the values to verify that the values also match.
 // If both key and the value matches, return true. Otherwise, all other instances will return false
 
-// GENERATE RANDOM TEST DATA
+// ----- GENERATE RANDOM TEST DATA -----
 // The function will receive a single integer as its parameter.
 // An empty array will hold the strings.
 // A variable, alpha, will hold the alphabet.
@@ -50,8 +50,8 @@ function match(object_1, object_2) {
 function random_data(integer) {
   var array = [];
   var alpha = 'abcdefghijklmnopqrstuvwxyz';
-  var string_length = Math.floor((Math.random() * 10) + 1);
   for (var x = 0; x < integer; x++) {
+    var string_length = Math.floor((Math.random() * 10) + 1);
     var new_string = '';
     for (var i = 0; i < string_length; i++) {
     new_string += alpha[Math.floor((Math.random() * 25))];
@@ -59,15 +59,21 @@ function random_data(integer) {
     array.push(new_string)
   }
   console.log(array)
+  return array
 }
 
 
-// DRIVER CODE
-// Longest phrase
-max_value(['tokyo', 'new york', 'paris', 'oslo', 'melbourne'])
-// Key-value match
-var alpha = {henry: 'poor', thomas: 'rich'}
-var beta = {evan: 'moderate', thomas: 'rich'}
-console.log(match(alpha, beta))
-// Random test data
-random_data(10)
+// ---- DRIVER CODE ----
+// --- Longest phrase ---
+// max_value(['tokyo', 'new york', 'paris', 'oslo', 'melbourne'])
+// --- Key-value match ---
+// var alpha = {henry: 'poor', thomas: 'rich'}
+// var beta = {evan: 'moderate', thomas: 'rich'}
+// console.log(match(alpha, beta))
+// --- Random test data ---
+// random_data(10)
+
+// Test code 10 times: generating an array of random lengths, printing the array, then finds the longest word and print the result.
+for (var i = 0; i < 10; i++) {
+  max_value(random_data(Math.floor((Math.random() * 15) + 1)));
+}

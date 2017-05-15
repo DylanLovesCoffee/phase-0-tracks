eeff9b5e335e526b7b0fd8c3e0ecfd22eb3f520d
DylanLovesCoffee <dyang825@gmail.com>
2017-05-15 00:42:26 -0400

    Create function that creates array with an array length of a given number, and each array string is between 1 and 10 random characters.

--- js/algos.js
+++ js/algos.js
@@ -9,9 +9,17 @@
 // If any keys match, loop through the values to verify that the values also match.
 // If both key and the value matches, return true. Otherwise, all other instances will return false
 
+// GENERATE RANDOM TEST DATA
+// The function will receive a single integer as its parameter.
+// An empty array will hold the strings.
+// A variable, alpha, will hold the alphabet.
+// Create a loop that will create a string by looping through the alpha variable.
+// Generate a random number between 1 and 10 that will determine the length of our string, and how many times we will loop through the alphabet.
+// Enclose our string creation loop with a new loop, where we will repeat the process above X amount of times. X being the integer given to the function.
+
 // Find longest phrase
 function max_value(array) {
-  char_length = []
+  var char_length = []
   for (var i = 0; i < array.length; i++) {
     char_length.push(array[i].length)
   }
@@ -38,12 +46,28 @@ function match(object_1, object_2) {
   }
 }
 
+// Generate random test data
+function random_data(integer) {
+  var array = [];
+  var alpha = 'abcdefghijklmnopqrstuvwxyz';
+  var string_length = Math.floor((Math.random() * 10) + 1);
+  for (var x = 0; x < integer; x++) {
+    var new_string = '';
+    for (var i = 0; i < string_length; i++) {
+    new_string += alpha[Math.floor((Math.random() * 25))];
+    }
+    array.push(new_string)
+  }
+  console.log(array)
+}
+
 
 // DRIVER CODE
 // Longest phrase
 max_value(['tokyo', 'new york', 'paris', 'oslo', 'melbourne'])
-
 // Key-value match
 var alpha = {henry: 'poor', thomas: 'rich'}
 var beta = {evan: 'moderate', thomas: 'rich'}
 console.log(match(alpha, beta))
+// Random test data
+random_data(10)

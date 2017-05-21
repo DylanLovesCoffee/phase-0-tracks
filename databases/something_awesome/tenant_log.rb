f8e963852fcc2fb19f205abc23f6abea3d309369
DylanLovesCoffee <dyang825@gmail.com>
2017-05-21 18:08:14 -0400

    Print table.

--- databases/something_awesome/tenant_log.rb
+++ databases/something_awesome/tenant_log.rb
@@ -1,7 +1,9 @@
 require 'sqlite3'
 require 'faker'
+require 'hirb'
 
 database = SQLite3::Database.new('tenants.db')
+
 
 create_table = <<-SQL
  CREATE TABLE IF NOT EXISTS tenants (
@@ -15,10 +17,13 @@ SQL
 
 database.execute(create_table)
 
-# database.execute("INSERT INTO tenants VALUES(null, 'testing', 10, 100, 'true')")
-
 def insert_tenant(database, name, floor, rent_due, rent_paid)
   database.execute("INSERT INTO tenants (name, floor, rent_due, rent_paid) VALUES (?, ?, ?, ?)", [name, floor, rent_due, rent_paid])
 end
 
-insert_tenant(database, Faker::Name.unique.name, Faker::Number.between(1, 28), Faker::Number.number(3), Faker::Boolean.boolean.to_s)
+# 10.times { insert_tenant(database, Faker::Name.unique.name, Faker::Number.between(1, 28), Faker::Number.number(3), Faker::Boolean.boolean.to_s) }
+
+# tenants_table is an Array
+tenants_table = database.execute("SELECT * FROM tenants")
+
+puts Hirb::Helpers::Table.render(tenants_table)

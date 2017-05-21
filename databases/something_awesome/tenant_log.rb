78b0eb3f19ee1ece25ba7c2165bb94ded72bbd5d
DylanLovesCoffee <dyang825@gmail.com>
2017-05-21 16:46:17 -0400

    Creates method to insert row. Requires faker gem.

--- databases/something_awesome/tenant_log.rb
+++ databases/something_awesome/tenant_log.rb
@@ -1,4 +1,5 @@
 require 'sqlite3'
+require 'faker'
 
 database = SQLite3::Database.new('tenants.db')
 
@@ -14,6 +15,10 @@ SQL
 
 database.execute(create_table)
 
-def insert_tenant(db, name, floor, rent_due, rent_paid)
+# database.execute("INSERT INTO tenants VALUES(null, 'testing', 10, 100, 'true')")
+
+def insert_tenant(database, name, floor, rent_due, rent_paid)
   database.execute("INSERT INTO tenants (name, floor, rent_due, rent_paid) VALUES (?, ?, ?, ?)", [name, floor, rent_due, rent_paid])
 end
+
+insert_tenant(database, Faker::Name.unique.name, Faker::Number.between(1, 28), Faker::Number.number(3), Faker::Boolean.boolean.to_s)

f3cc14fa973f2c61399f535052202af1ad8997f3
DylanLovesCoffee <dyang825@gmail.com>
2017-05-26 01:20:58 -0400

    Add event listener

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,15 @@
 console.log("Hello friends! I'm printing in the console!")
+
+var corgi = document.getElementById("brown pup");
+var pug = document.getElementById("black pup");
+
+corgi.onclick = function() {
+  console.log("Corgi has been clicked");
+  corgi.style.border = "2px dashed brown";
+  corgi.insertAdjacentHTML("beforebegin", "<p>Corgi thanks you for petting him</p>")
+}
+
+pug.onclick = function() {
+  console.log("Pug has been clicked");
+  pug.insertAdjacentHTML('afterend', '<img src="https://media.giphy.com/media/DTgZq3XBUwQgM/giphy.gif">')
+}

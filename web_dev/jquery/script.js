f74e9e9b7550c3efe6b7f5ce4791ee0837cd51b6
DylanLovesCoffee <dyang825@gmail.com>
2017-05-27 17:44:53 -0400

    Hide red ball. Give alert to correct answer and show red ball.

--- web_dev/jquery/script.js
+++ web_dev/jquery/script.js
@@ -3,8 +3,17 @@ $(document).ready(function(){
   $("button").click(function(){
     $("#ball").animate({left:"250px"});
     $("#ball").hide("slow", "linear", function(){
-      $(".boxes").animate({left:"150px"});
+      $("#ball").animate({left:"150px"})
     });
   })
-
+  $("#box_1").click(function(){
+    alert("Nope! Try again.");
+  })
+  $("#box_2").click(function(){
+    $("#ball").show();
+    alert("You got it! Congrats!");
+  })
+  $("#box_3").click(function(){
+    alert("Nope! Try again.");
+  })
 });
